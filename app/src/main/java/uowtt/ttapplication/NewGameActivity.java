2b6e067d9799dfc6540f3909b2e4cf488ad82c38
Jozhogg <jmhogg93@googlemail.com>
2015-08-21 16:37:17 +0100

    Added match validation and confirmation text to new Match submission (and recovered from hard drive corruption... :( )

--- app/src/main/java/uowtt/ttapplication/NewGameActivity.java
+++ app/src/main/java/uowtt/ttapplication/NewGameActivity.java
@@ -1,34 +1,43 @@
 package uowtt.ttapplication;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.internal.widget.AdapterViewCompat;
 import android.text.Editable;
+import android.text.Html;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Arrays;
+import java.util.List;
 
 
 public class NewGameActivity extends Activity {
+
+    String[] playerA;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_game);
 
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        final Spinner spinner = (Spinner) findViewById(R.id.spinner);
 
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                                R.array.scores_array, android.R.layout.simple_spinner_item);
+                R.array.scores_array, android.R.layout.simple_spinner_item);
 
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
@@ -45,8 +54,10 @@ public class NewGameActivity extends Activity {
 
         final String[] playerList = b.getStringArray("playerNamesList");
 
+        playerA = playerList;
+
         ArrayAdapter<String> auto_adapter = new ArrayAdapter<String>(this,
-                                        R.layout.support_simple_spinner_dropdown_item, playerList);
+                R.layout.support_simple_spinner_dropdown_item, playerList);
         chal_text.setAdapter(auto_adapter);
         oppo_text.setAdapter(auto_adapter);
 
@@ -62,10 +73,11 @@ public class NewGameActivity extends Activity {
 
             @Override
             public void afterTextChanged(Editable s) {
-                if(s.length() > 2 & !Arrays.asList(playerList).contains(s.toString())) {
+                if (s.length() > 2 & !Arrays.asList(playerList).contains(s.toString())) {
                     chal_text.setText(s.subSequence(0, 2));
                     chal_text.setSelection(chal_text.getText().length());
                 }
+                attemptDisplayMatch(spinner, chal_text, oppo_text);
             }
         };
 
@@ -81,15 +93,54 @@ public class NewGameActivity extends Activity {
 
             @Override
             public void afterTextChanged(Editable s) {
-                if(s.length() > 2 & !Arrays.asList(playerList).contains(s.toString())) {
+                if (s.length() > 2 & !Arrays.asList(playerList).contains(s.toString())) {
                     oppo_text.setText(s.subSequence(0, 2));
                     oppo_text.setSelection(chal_text.getText().length());
                 }
+                attemptDisplayMatch(spinner, chal_text, oppo_text);
             }
         };
 
         chal_text.addTextChangedListener(tw_c);
         oppo_text.addTextChangedListener(tw_o);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
+                attemptDisplayMatch(spinner, chal_text, oppo_text);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                return;
+            }
+        });
+    }
+
+    private void attemptDisplayMatch(Spinner spinner, AutoCompleteTextView chal_text, AutoCompleteTextView oppo_text) {
+
+        String chal_name = chal_text.getText().toString();
+        String oppo_name = oppo_text.getText().toString();
+        String score = spinner.getSelectedItem().toString();
+
+        TextView outcome = (TextView) findViewById(R.id.outcome_text);
+
+        if(validate_match(chal_name, oppo_name, score, false)){
+
+            if(score.equals("2-0") || score.equals("2-1")){
+
+                outcome.setText(Html.fromHtml("<font color=green>"+chal_name+"<font color=black>"+
+                        " beats "+"<font color=red>"+oppo_name+" "+"<font color=black>"+score));
+            }
+            else{
+                outcome.setText(Html.fromHtml("<font color=green>"+oppo_name+"<font color=black>"+
+                        " beats "+"<font color=red>"+chal_name+" "+"<font color=black>"+score));
+            }
+        }
+        else{
+            outcome.setText("");
+        }
+
     }
 
     public void submit(View view){
@@ -102,15 +153,44 @@ public class NewGameActivity extends Activity {
         String oppo_name = oppo_text.getText().toString();
         String score = spinner.getSelectedItem().toString();
 
-        Intent intent = new Intent();
+        if(validate_match(chal_name, oppo_name, score, true)) {
+            Intent intent = new Intent();
 
-        intent.putExtra("chal", chal_name);
-        intent.putExtra("oppo", oppo_name);
-        intent.putExtra("score", score);
+            intent.putExtra("chal", chal_name);
+            intent.putExtra("oppo", oppo_name);
+            intent.putExtra("score", score);
 
-        setResult(1, intent);
+            setResult(1, intent);
 
-        finish();
+            finish();
+        }
 
+    }
+
+    private boolean validate_match(String chal_name, String oppo_name, String score, boolean toasts) {
+
+        Context context = getApplicationContext();
+        List<String> playerL = Arrays.asList(playerA);
+
+        if(score == "---"){
+
+            Toast toast = Toast.makeText(context, "Please select valid score", Toast.LENGTH_LONG);
+            toast.show();
+
+            return false;
+        }
+
+        int c_pos = playerL.indexOf(chal_name);
+        int o_pos = playerL.indexOf(oppo_name);
+
+        if(c_pos < o_pos){
+
+            Toast toast = Toast.makeText(context, "A player cannot challenge somebody below them on the ladder", Toast.LENGTH_LONG);
+            toast.show();
+
+            return false;
+        }
+
+        return true;
     }
 }
