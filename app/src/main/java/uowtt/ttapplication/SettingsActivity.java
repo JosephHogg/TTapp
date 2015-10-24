23df8e49dc6bb63b3a533f3efa0b544ea5f41d1a
Joseph Hogg <jmhogg93@googlemail.com>
2015-10-24 15:38:31 +0100

    Added reset ladder dialog and password check

--- app/src/main/java/uowtt/ttapplication/SettingsActivity.java
+++ app/src/main/java/uowtt/ttapplication/SettingsActivity.java
@@ -1,5 +1,7 @@
 package uowtt.ttapplication;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,6 +11,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -60,9 +63,36 @@ public class SettingsActivity extends AppCompatActivity {
 
     public void newLadder(View v){
 
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.putExtra("reset", true);
+        final EditText edit = new EditText(SettingsActivity.this);
 
-        startActivity(intent);
+        new AlertDialog.Builder(SettingsActivity.this)
+                .setTitle("Are you sure?")
+                .setMessage("Are you sure you're ready to reset the ladder (The current ladder will be archived).")
+                .setView(edit)
+                .setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // continue with reset
+
+                        Log.d("pass", edit.getText().toString());
+
+                        if (edit.getText().toString().equals("password")) {
+
+                            Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
+
+                            intent.putExtra("reset", true);
+
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(SettingsActivity.this, "Incorrect Password", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int width) {
+                        //do nothing
+                    }
+                })
+                .setIcon(android.R.drawable.stat_sys_warning)
+                .show();
     }
 }
